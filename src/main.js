0798177a060c74238e332646f08d2944f5052014
lixiang <271896700@qq.com>
2025-11-29 09:57:02 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn,
+})
+app.mount('#app')
